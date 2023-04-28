c8e02116cd97d145affa77faaadccd62390325ee
manoj <gandhamanojkutty@gmail.com>
2023-04-28 16:32:48 +0530

    servers.tf

---
+++ servers.tf
@@ -0,0 +1,38 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_instance" "MongoDB" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "MongoDB"
+  }
+}
+
+resource "aws_instance" "Catalogue" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Catalogue"
+  }
+}
+
+resource "aws_instance" "Redis" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Redis"
+  }
+}
+
+
+

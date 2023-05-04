565997cdeefd05afd4f143e14ddbe7fae0587dd7
manoj <gandhamanojkutty@gmail.com>
2023-05-04 11:16:05 +0530

    servers.tf

--- servers.tf
+++ servers.tf
@@ -4,41 +4,68 @@ data "aws_ami" "centos" {
   name_regex       = "Centos-8-DevOps-Practice"
 }
 
-resource "aws_instance" "frontend" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "frontend"
-  }
+data "aws_security_group" "allow-all" {
+  name = "allow-all"
 }
 
-resource "aws_instance" "MongoDB" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "MongoDB"
-  }
-}
-
-resource "aws_instance" "Catalogue" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "Catalogue"
-  }
-}
-
-resource "aws_instance" "Redis" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "Redis"
-  }
+variable "instance_type" {
+  default = "t3.small"
 }
 
 
+variable "components" {
+  default = {
+    frontend = {
+      name          = "frontend"
+      instance_type = "t3.small"
+    }
+    mongodb = {
+      name          = "mongodb"
+      instance_type = "t3.small"
+    }
+    catalogue = {
+      name          = "catalogue"
+      instance_type = "t3.micro"
+    }
+    redis = {
+      name          = "redis"
+      instance_type = "t3.small"
+    }
+    user = {
+      name          = "user"
+      instance_type = "t3.micro"
+    }
+    cart = {
+      name          = "cart"
+      instance_type = "t3.micro"
+    }
+    mysql = {
+      name          = "mysql"
+      instance_type = "t3.small"
+    }
+    shipping = {
+      name          = "shipping"
+      instance_type = "t3.medium"
+    }
+    rabbitmq = {
+      name          = "rabbitmq"
+      instance_type = "t3.small"
+    }
+    payment = {
+      name          = "payment"
+      instance_type = "t3.small"
+    }
+  }
+}
 
+
+resource "aws_instance" "instance" {
+  for_each               = var.components
+  ami                    = data.aws_ami.centos.image_id
+  instance_type          = each.value["instance_type"]
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+
+  tags = {
+    Name = each.value["name"]
+  }
+}

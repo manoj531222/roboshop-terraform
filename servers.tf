d9c7e50105931676ff28db79dfbf04f75c8a1c40
manoj <gandhamanojkutty@gmail.com>
2023-05-07 12:22:13 +0530

    code updated

--- servers.tf
+++ servers.tf
@@ -1,9 +1,71 @@
-module "servers" {
-  for_each = var.components
+data "aws_ami" "centos" {
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
+data "aws_security_group" "allow-all" {
+  name = "allow-all"
+}
+variable "components" {
+  default = {
+    frontend = {
+      name          = "frontend"
+      instance_type = "t3.small"
+    }
+    mongodb = {
+      name          = "mongodb"
+      instance_type = "t3.small"
+    }
+    catalogue = {
+      name          = "catalogue"
+      instance_type = "t3.micro"
+    }
+    redis = {
+      name          = "redis"
+      instance_type = "t3.small"
+    }
+    user = {
+      name          = "user"
+      instance_type = "t3.micro"
+    }
+    cart = {
+      name          = "cart"
+      instance_type = "t3.micro"
+    }
+    mysql = {
+      name          = "mysql"
+      instance_type = "t3.small"
+    }
+    shipping = {
+      name          = "shipping"
+      instance_type = "t3.medium"
+    }
+    rabbitmq = {
+      name          = "rabbitmq"
+      instance_type = "t3.small"
+    }
+    payment = {
+      name          = "payment"
+      instance_type = "t3.small"
+    }
+  }
+}
+resource "aws_instance" "instance" {
+  for_each               = var.components
+  ami                    = data.aws_ami.centos.image_id
+  instance_type          = each.value["instance_type"]
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+  tags = {
+    Name = each.value["name"]
+  }
+}
 
-  source         = "./module"
-  component_name = each.value["name"]
-  env            = var.env
-  instance_type  = each.value["instance_type"]
-  password       = lookup(each.value, "password", "null")
+
+resource "aws_route53_record" "records" {
+  for_each = var.components
+  zone_id  = "Z03986262CQPCHNJNZM9L"
+  name     = "${each.value["name"]}-dev.rdevopsb72.online"
+  type     = "A"
+  ttl      = 30
+  records  = [aws_instance.instance[each.value["name"]].private_ip]
 }
--- state.tf
+++ state.tf
@@ -1,3 +1,3 @@
-terraform {
-  backend "s3" {}
-}
+bucket = "terraform-a72"
+key    = "roboshop/prod/terraform.tfstate"
+region = "us-east-1"

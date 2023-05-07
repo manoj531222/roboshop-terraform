b18b9dcb79be5f682604d02bb64cc5903aa1cc19
manoj <gandhamanojkutty@gmail.com>
2023-05-07 12:32:40 +0530

    code updated

--- state.tf
+++ state.tf
@@ -1,3 +1,7 @@
-bucket = "terraform-a72"
-key    = "roboshop/prod/terraform.tfstate"
-region = "us-east-1"
+terraform {
+  backend "s3" {
+    bucket = "mdevops333"
+    key    = "roboshop/dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+  }

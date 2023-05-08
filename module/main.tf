305266deb8ca791bf2def9597827bbb0e1c991c8
manoj <gandhamanojkutty@gmail.com>
2023-05-08 13:23:16 +0530

    password updated

--- module/main.tf
+++ module/main.tf
@@ -31,8 +31,8 @@ resource "null_resource" "provisioner" {
 
 
 resource "aws_route53_record" "records" {
-  zone_id = "Z03986262CQPCHNJNZM9L"
-  name    = "${var.component_name}-dev.rdevopsb72.online"
+  zone_id = "Z091937226LTVI5BTYORY"
+  name    = "${var.component_name}-dev.mdevops333.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.instance.private_ip]

ba0536a1f31e796dee111e06f7725418a6362b8c
manoj <gandhamanojkutty@gmail.com>
2023-05-08 13:15:11 +0530

    password updated

--- module/main.tf
+++ module/main.tf
@@ -1,6 +1,3 @@
-variable "instace_type" {
-  default = ""
-}
 resource "aws_instance" "instance" {
   ami                    = data.aws_ami.centos.image_id
   instance_type          = var.instance_type
@@ -11,17 +8,18 @@ resource "aws_instance" "instance" {
   }
 }
 
-resource "null_resource" "provisiner" {
+resource "null_resource" "provisioner" {
 
-  depends_on = [aws_instance.instance,aws_route53_record.records]
-
+  depends_on = [aws_instance.instance, aws_route53_record.records]
   provisioner "remote-exec" {
+
     connection {
       type     = "ssh"
       user     = "centos"
       password = "DevOps321"
       host     = aws_instance.instance.private_ip
     }
+
     inline = [
       "rm -rf roboshop-shell",
       "git clone https://github.com/manoj531222/roboshop-shell",
@@ -31,10 +29,11 @@ resource "null_resource" "provisiner" {
   }
 }
 
+
 resource "aws_route53_record" "records" {
-  zone_id  = "Z091937226LTVI5BTYORY"
-  name     = "${var.component_name}-dev.mdevops333.online"
-  type     = "A"
-  ttl      = 30
-  records  = [aws_instance.instance[each.value["name"]].private_ip]
+  zone_id = "Z03986262CQPCHNJNZM9L"
+  name    = "${var.component_name}-dev.rdevopsb72.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instance.private_ip]
 }

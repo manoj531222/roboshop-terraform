fd635bd214916b27b4f54fdc06200c320c1d36f4
manoj <gandhamanojkutty@gmail.com>
2023-08-16 14:19:21 +0530

    dns records

--- servers.tf
+++ servers.tf
@@ -28,6 +28,13 @@ resource "aws_instance" "mongodb" {
     Name = "mongodb"
   }
 }
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z0514347LLZOA0DD7NAB"
+  name    = "mongodb-dev.mdevops333.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
+}
 
 resource "aws_instance" "catalogue" {
   ami           = data.aws_ami.centos.image_id
@@ -36,6 +43,13 @@ resource "aws_instance" "catalogue" {
   tags = {
     Name = "catalogue"
   }
+}
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z0514347LLZOA0DD7NAB"
+  name    = "catalogue-dev.mdevops333.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
 }
 
 resource "aws_instance" "redis" {
@@ -46,6 +60,13 @@ resource "aws_instance" "redis" {
     Name = "redis"
   }
 }
+resource "aws_route53_record" "redis" {
+  zone_id = "Z0514347LLZOA0DD7NAB"
+  name    = "redis-dev.mdevops333.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
+}
 
 resource "aws_instance" "user" {
   ami           = data.aws_ami.centos.image_id
@@ -54,6 +75,13 @@ resource "aws_instance" "user" {
   tags = {
     Name = "user"
   }
+}
+resource "aws_route53_record" "user" {
+  zone_id = "Z0514347LLZOA0DD7NAB"
+  name    = "user-dev.mdevops333.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
 }
 
 resource "aws_instance" "cart" {
@@ -64,6 +92,13 @@ resource "aws_instance" "cart" {
     Name = "cart"
   }
 }
+resource "aws_route53_record" "cart" {
+  zone_id = "Z0514347LLZOA0DD7NAB"
+  name    = "cart-dev.mdevops333.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
+}
 
 resource "aws_instance" "mysql" {
   ami           = data.aws_ami.centos.image_id
@@ -72,6 +107,13 @@ resource "aws_instance" "mysql" {
   tags = {
     Name = "mysql"
   }
+}
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z0514347LLZOA0DD7NAB"
+  name    = "mysql-dev.mdevops333.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
 }
 
 resource "aws_instance" "shipping" {
@@ -82,6 +124,13 @@ resource "aws_instance" "shipping" {
     Name = "shipping"
   }
 }
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z0514347LLZOA0DD7NAB"
+  name    = "shipping-dev.mdevops333.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
+}
 
 resource "aws_instance" "rabbitmq" {
   ami           = data.aws_ami.centos.image_id
@@ -91,6 +140,13 @@ resource "aws_instance" "rabbitmq" {
     Name = "rabbitmq"
   }
 }
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z0514347LLZOA0DD7NAB"
+  name    = "rabbitmq-dev.mdevops333.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
+}
 
 resource "aws_instance" "payment" {
   ami           = data.aws_ami.centos.image_id
@@ -99,4 +155,11 @@ resource "aws_instance" "payment" {
   tags = {
     Name = "payment"
   }
+}
+resource "aws_route53_record" "payment" {
+  zone_id = "Z0514347LLZOA0DD7NAB"
+  name    = "payment-dev.mdevops333.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
 }

655f068abe2fe945ca40498591772038a5839c2a
manoj <gandhamanojkutty@gmail.com>
2023-08-17 12:02:59 +0530

    count.index

--- servers.tf
+++ servers.tf
@@ -16,16 +16,15 @@ variable "components" {
 }
 
 resource "aws_instance" "instance" {
-  count         = length(var.components)
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+  count                  = length(var.components)
+  ami                    = data.aws_ami.centos.image_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+
+  tags = {
+    Name = var.components[count.index]
+  }
 }
-
-
-#  tags = {
-#    Name = var.components[count.index]
-#  }
 #
 #resource "aws_route53_record" "frontend" {
 #  zone_id = "Z0514347LLZOA0DD7NAB"

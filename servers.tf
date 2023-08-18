f8a05e13450cf9886cd12159c9a655c1d7545c27
manoj <gandhamanojkutty@gmail.com>
2023-08-18 14:48:16 +0530

    shell commands added

--- servers.tf
+++ servers.tf
@@ -9,6 +9,28 @@ resource "aws_instance" "instance" {
     Name = each.value["name"]
   }
 }
+resource "null_resource" "provisioner" {
+
+  depends_on = [aws_instance.instance, aws_route53_record.records]
+  for_each   = var.components
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = DevOps321
+      host     = aws_instance.instance[each.value["name"]].private_ip
+    }
+
+    inline = [
+      "rm -rf roboshop-shell",
+      "git clone https://github.com/manoj531222/roboshop-shell.git",
+      "cd roboshop-shell",
+      "sudo bash ${each.value["name"]}.sh"
+
+    ]
+  }
+}
 
 resource "aws_route53_record" "records" {
   for_each = var.components

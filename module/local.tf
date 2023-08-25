832a6af0eb51d063dc009df9c8cfd0a2227d3002
manoj <gandhamanojkutty@gmail.com>
2023-08-25 22:38:41 +0530

    code updated

--- module/local.tf
+++ module/local.tf
@@ -7,8 +7,7 @@ locals {
     "sudo bash ${var.component_name}.sh ${var.password}"
   ]
   app_commands = [
-#    "sudo labauto ansible",
-#    "ansible-pull -i localhost, -U https://github.com/mdevops333/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
-    "echo ok"
+    "sudo labauto ansible",
+    "ansible-pull -i localhost, -U https://github.com/mdevops333/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
   ]
 }

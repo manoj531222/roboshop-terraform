d9129d617ad8a4db73eaf243db69774c72ef1604
manoj <gandhamanojkutty@gmail.com>
2023-08-25 22:05:14 +0530

    aws_iam_role_policy" "ssm-ps-policy

--- module/main.tf
+++ module/main.tf
@@ -3,38 +3,32 @@ resource "aws_instance" "instance" {
   instance_type          = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
+  iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
   tags = {
     Name = local.name
   }
 }
-
 resource "null_resource" "provisioner" {
   depends_on = [aws_instance.instance, aws_route53_record.records]
   provisioner "remote-exec" {
-
     connection {
       type     = "ssh"
       user     = "centos"
       password = "DevOps321"
       host     = aws_instance.instance.private_ip
     }
-
     inline = var.app_type == "db" ? local.db_commands : local.app_commands
   }
 }
-
-
 resource "aws_route53_record" "records" {
-  zone_id = "Z0514347LLZOA0DD7NAB"
-  name    = "${var.component_name}-dev.mdevops333.online"
+  zone_id = "Z03986262CQPCHNJNZM9L"
+  name    = "${var.component_name}-dev.mdevops.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.instance.private_ip]
 }
-
-resource "aws_iam_role" "test_role" {
+resource "aws_iam_role" "role" {
   name = "${var.component_name}-${var.env}-role"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -48,36 +42,42 @@ resource "aws_iam_role" "test_role" {
       },
     ]
   })
-
   tags = {
     tag-key = "${var.component_name}-${var.env}-role"
   }
 }
 
-resource "aws_iam_role_policy" "ssm-ps-policy" {
-  name = "${var.component_name}-${var.env}-ssm-ps-policy"
-  role = aws_iam_role.test_role.id
 
-  policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Sid": "VisualEditor0",
-        "Effect": "Allow",
-        "Action": [
-          "ssm:GetParameterHistory",
-          "ssm:GetParametersByPath",
-          "ssm:GetParameters",
-          "ssm:GetParameter"
-        ],
-        "Resource": "arn:aws:ssm:us-east-1:693381842268:parameter/${var.env}.${var.component_name}.*"
-      },
-      {
-        "Sid": "VisualEditor1",
-        "Effect": "Allow",
-        "Action": "ssm:DescribeParameters",
-        "Resource": "*"
-      }
-    ]
-  })
-}
+  resource "aws_iam_instance_profile" "instance_profile" {
+    name = "${var.component_name}-${var.env}-role"
+    role = aws_iam_role.role.name
+  }
+
+  resource "aws_iam_role_policy" "ssm-ps-policy" {
+    name = "${var.component_name}-${var.env}-ssm-ps-policy"
+    role = aws_iam_role.role.id
+
+
+    policy = jsonencode({
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "VisualEditor0",
+          "Effect" : "Allow",
+          "Action" : [
+            "ssm:GetParameterHistory",
+            "ssm:GetParametersByPath",
+            "ssm:GetParameters",
+            "ssm:GetParameter"
+          ],
+          "Resource" : "arn:aws:ssm:us-east-1:633788536644:parameter/${var.env}.${var.component_name}.*"
+        },
+        {
+          "Sid" : "VisualEditor1",
+          "Effect" : "Allow",
+          "Action" : "ssm:DescribeParameters",
+          "Resource" : "*"
+        }
+      ]
+    })
+  }

792585bc45c5303c22ed8d8a978849199127beec
manoj <gandhamanojkutty@gmail.com>
2023-09-05 13:12:07 +0530

    vpc

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ module "vpc" {
   source = "git::https://github.com/manoj531222/tf-module-vpc.git"
   for_each = var.vpc
   cidr_block = each.value["cidr_block"]
-  tags = var.tags
+#  tags = var.tags
 }

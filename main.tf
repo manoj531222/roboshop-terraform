4ed2a60e2631875ed52446fca79084937e812402
manoj <gandhamanojkutty@gmail.com>
2023-09-06 14:59:15 +0530

    subnets added

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "git::https://github.com/manoj53122/tf-module-vpc.git"
+  source = "git::https://github.com/manoj531222/tf-module-vpc.git"
 
   for_each   = var.vpc
   cidr_block = each.value["cidr_block"]

be7a93aa305703b242abe88581b89e26a94340ba
manoj <gandhamanojkutty@gmail.com>
2023-09-07 14:03:19 +0530

    asg added

--- main.tf
+++ main.tf
@@ -11,18 +11,17 @@ module "vpc" {
 module "app" {
   source = "git::https://github.com/manoj531222/tf-module-app.git"
 
-  for_each = var.app
-  instance_type = each.value["instance_type"]
-  name = each.value["name"]
+  for_each         = var.app
+  instance_type    = each.value["instance_type"]
+  name             = each.value["name"]
   desired_capacity = each.value["desired_capacity"]
-  max_size = each.value["max_size"]
-  min_size = each.value["min_size"]
+  max_size         = each.value["max_size"]
+  min_size         = each.value["min_size"]
 
-  env = var.env
+  env          = var.env
   bastion_cidr = var.bastion_cidr
 
-
   subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
-  vpc_id = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
   allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
 }

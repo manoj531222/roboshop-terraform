bd94059a6aef8b58e39d3d7da3479f46186511eb
manoj <gandhamanojkutty@gmail.com>
2023-09-07 15:08:00 +0530

    asg added

--- main.tf
+++ main.tf
@@ -17,10 +17,11 @@ module "app" {
   desired_capacity = each.value["desired_capacity"]
   max_size         = each.value["max_size"]
   min_size         = each.value["min_size"]
-  tags = local.tags
+
 
   env          = var.env
   bastion_cidr = var.bastion_cidr
+  tags = local.tags
 
   subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
   vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)

67ccd6d0ae7935343d5388070efbd4a86a637024
manoj <gandhamanojkutty@gmail.com>
2023-09-09 11:40:51 +0530

    dbs

--- vars.tf
+++ vars.tf
@@ -7,5 +7,10 @@ variable "default_vpc_cidr" {}
 variable "default_vpc_rtid" {}
 variable "docdb" {}
 variable "kms_arn" {}
+variable "rds" {}
+variable "elasticache" {}
+variable "rabbitmq" {}
+variable "domain_id" {}
+
 
 

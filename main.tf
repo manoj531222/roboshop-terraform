54262a2a9cbb2756784de0220920c42f0b080a40
manoj <gandhamanojkutty@gmail.com>
2023-09-09 11:50:45 +0530

    dbs

--- main.tf
+++ main.tf
@@ -67,23 +67,6 @@ module "rds" {
   kms_arn = var.kms_arn
 }
 
-module "rds" {
-  source = "git::https://github.com/manoj531222/tf-module-rds.git"
-
-  for_each      = var.rds
-  subnets       = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
-  allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
-  engine_version = each.value["engine_version"]
-  instance_count = each.value["instance_count"]
-  instance_class = each.value["instance_class"]
-
-
-  tags    = local.tags
-  env     = var.env
-  vpc_id  = local.vpc_id
-  kms_arn = var.kms_arn
-}
-
 module "elasticache" {
   source = "git::https://github.com/manoj531222/tf-module-elasticache.git"
 

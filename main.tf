65adc51da16850821d81a9d28d581593a7d623f3
manoj <gandhamanojkutty@gmail.com>
2023-09-29 12:47:48 +0530

    load runner added

--- main.tf
+++ main.tf
@@ -128,3 +128,37 @@ module "app" {
 
 
 }
+
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+
+}
+
+resource "aws_instance" "load" {
+  ami                    = data.aws_ami.ami.id
+  instance_type          = "t3.medium"
+  vpc_security_group_ids = ["sg-08bcece2af26f1c2b"]
+  tags = {
+    Name = "load-runner"
+  }
+}
+
+resource "null_resource" "load" {
+  provisioner "remote-exec" {
+
+    connection {
+      host     = aws_instance.load.private_ip
+      user     = "root"
+      password = "DevOps321"
+    }
+
+    inline = [
+      "curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/docker/install.sh | bash",
+      "docker pull robotshop/rs-load"
+    ]
+
+
+  }
+}

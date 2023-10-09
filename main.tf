0eba4bad25a410d419077a59a8ca3cce5362383a
manoj <gandhamanojkutty@gmail.com>
2023-10-09 18:11:51 +0530

    module eks added

--- main.tf
+++ main.tf
@@ -12,74 +12,74 @@ module "vpc" {
   default_vpc_rtid = var.default_vpc_rtid
 }
 
-module "docdb" {
-  source = "git::https://github.com/manoj531222/tf-module-docdb.git"
-
-  for_each      = var.docdb
-  subnets       = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
-  allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
-  engine_version = each.value["engine_version"]
-  instance_count = each.value["instance_count"]
-  instance_class = each.value["instance_class"]
-
-
-  tags    = local.tags
-  env     = var.env
-  vpc_id  = local.vpc_id
-  kms_arn = var.kms_arn
-}
-
-module "rds" {
-  source = "git::https://github.com/manoj531222/tf-module-rds.git"
-
-  for_each      = var.rds
-  subnets       = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
-  allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
-  engine_version = each.value["engine_version"]
-  instance_count = each.value["instance_count"]
-  instance_class = each.value["instance_class"]
-
-
-  tags    = local.tags
-  env     = var.env
-  vpc_id  = local.vpc_id
-  kms_arn = var.kms_arn
-}
-
-module "elasticache" {
-  source = "git::https://github.com/manoj531222/tf-module-elasticache.git"
-
-  for_each                = var.elasticache
-  subnets                 = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
-  allow_db_cidr           = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
-  engine_version          = each.value["engine_version"]
-  replicas_per_node_group = each.value["replicas_per_node_group"]
-  num_node_groups         = each.value["num_node_groups"]
-  node_type               = each.value["node_type"]
-
-
-  tags    = local.tags
-  env     = var.env
-  vpc_id  = local.vpc_id
-  kms_arn = var.kms_arn
-}
-
-module "rabbitmq" {
-  source = "git::https://github.com/manoj531222/tf-module-amazon-mq.git"
-
-  for_each      = var.rabbitmq
-  subnets       = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
-  allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
-  instance_type = each.value["instance_type"]
-
-
-  tags         = local.tags
-  env          = var.env
-  vpc_id       = local.vpc_id
-  kms_arn      = var.kms_arn
-  bastion_cidr = var.bastion_cidr
-  domain_id = var.domain_id
-}
+#module "docdb" {
+#  source = "git::https://github.com/manoj531222/tf-module-docdb.git"
+#
+#  for_each      = var.docdb
+#  subnets       = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+#  allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
+#  engine_version = each.value["engine_version"]
+#  instance_count = each.value["instance_count"]
+#  instance_class = each.value["instance_class"]
+#
+#
+#  tags    = local.tags
+#  env     = var.env
+#  vpc_id  = local.vpc_id
+#  kms_arn = var.kms_arn
+#}
+#
+#module "rds" {
+#  source = "git::https://github.com/manoj531222/tf-module-rds.git"
+#
+#  for_each      = var.rds
+#  subnets       = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+#  allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
+#  engine_version = each.value["engine_version"]
+#  instance_count = each.value["instance_count"]
+#  instance_class = each.value["instance_class"]
+#
+#
+#  tags    = local.tags
+#  env     = var.env
+#  vpc_id  = local.vpc_id
+#  kms_arn = var.kms_arn
+#}
+#
+#module "elasticache" {
+#  source = "git::https://github.com/manoj531222/tf-module-elasticache.git"
+#
+#  for_each                = var.elasticache
+#  subnets                 = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+#  allow_db_cidr           = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
+#  engine_version          = each.value["engine_version"]
+#  replicas_per_node_group = each.value["replicas_per_node_group"]
+#  num_node_groups         = each.value["num_node_groups"]
+#  node_type               = each.value["node_type"]
+#
+#
+#  tags    = local.tags
+#  env     = var.env
+#  vpc_id  = local.vpc_id
+#  kms_arn = var.kms_arn
+#}
+#
+#module "rabbitmq" {
+#  source = "git::https://github.com/manoj531222/tf-module-amazon-mq.git"
+#
+#  for_each      = var.rabbitmq
+#  subnets       = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+#  allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
+#  instance_type = each.value["instance_type"]
+#
+#
+#  tags         = local.tags
+#  env          = var.env
+#  vpc_id       = local.vpc_id
+#  kms_arn      = var.kms_arn
+#  bastion_cidr = var.bastion_cidr
+#  domain_id = var.domain_id
+#}
 
 #module "alb" {
 #  source = "git::https://github.com/manoj531222/tf-module-alb.git"
@@ -131,6 +131,7 @@ module "rabbitmq" {
 module "eks" {
   source             = "github.com/r-devops/tf-module-eks"
   ENV                = var.env
+  version = 1.27
   PRIVATE_SUBNET_IDS = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "app", null), "subnet_ids", null)
   PUBLIC_SUBNET_IDS  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), "public", null), "subnet_ids", null)
   DESIRED_SIZE       = 2

68a139aa759d6e25d777ce565be0ca14ee53f607
manoj <gandhamanojkutty@gmail.com>
2023-10-16 12:25:06 +0530

    test output for eks module

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,6 @@
-output "vpc" {
-  value = lookup(lookup(module.vpc, "main", null), "subnets", null)
+#output "vpc" {
+#  value = lookup(lookup(module.vpc, "main", null), "subnets", null)
+#}
+output "test" {
+  value = module.eks
 }
